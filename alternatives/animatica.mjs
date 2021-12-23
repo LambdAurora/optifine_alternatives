674dfdf85d96b15fcee7d898908036c682d39be9
LambdAurora <email@lambdaurora.dev>
2021-12-23 21:25:52 +0100

    Update animatica version list.

--- alternatives/animatica.mjs
+++ alternatives/animatica.mjs
@@ -5,7 +5,7 @@ export default new Mod(
 	"FoundationGames",
 	"A mod implementing the OptiFine/MCPatcher animated texture format.",
 )
-.add_version(17)
+.add_version(17, 18)
 .add_category("Cosmetic")
 .add_link(
 	{ host: "curseforge" },
