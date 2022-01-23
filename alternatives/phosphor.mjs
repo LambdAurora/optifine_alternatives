c23e9f05cfa68c9437d92d9dfb436569fb20ed41
Mihail Yaremenko <m50421@outlook.com>
2022-01-23 16:54:36 +0200

    Add mod incompatibility notes (#10)
    Add notes that Phosphor and Starlight are incompatible with each other.

--- alternatives/phosphor.mjs
+++ alternatives/phosphor.mjs
@@ -3,7 +3,7 @@ import Mod from "../build_src/mod.mjs";
 export default new Mod(
 	"Phosphor",
 	"CaffeineMC",
-	"No-compromises lighting engine optimization mod.",
+	"No-compromises lighting engine optimization mod. Incompatible with Starlight.",
 )
 .add_version({id: 12, note: "Forge"}, 15, 16, 17, 18)
 .add_category("Performance", "General")
