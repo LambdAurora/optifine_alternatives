eaa51c15ed95d4c3a39bbee25687f0025397a807
Traben <ben.trabant@gmail.com>
2022-02-19 21:53:48 +1000

    Update entity-texture-features.mjs

--- alternatives/entity-texture-features.mjs
+++ alternatives/entity-texture-features.mjs
@@ -3,7 +3,7 @@ import Mod from "../build_src/mod.mjs";
 export default new Mod(
 	"Entity Texture Features",
 	"Traben_0",
-	"A Fabric mod implementing randomized & emissive texture support for mobs set by the resourcepack. Fully compatible with the optifine format.",
+	"A Fabric mod implementing randomized & emissive texture support for mobs set by the resourcepack. Fully compatible with the Optifine format & Sodium.",
 )
 .add_version(16, 17, 18)
 .add_category("Cosmetic")
