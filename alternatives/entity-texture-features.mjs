eba92f553b2f9326b7aa03b63deff88bee7bd2df
Traben <benjamin.trabant@my.jcu.edu.au>
2022-02-19 22:59:33 +1000

    Entity Texture Features mod added (#12)
    * adding Entity Texture Features mod
    
    second times the charm
    
    * Update README.in.md
    
    * Update entity-texture-features.mjs
    
    * Update README.in.md

---
+++ alternatives/entity-texture-features.mjs
@@ -0,0 +1,14 @@
+import Mod from "../build_src/mod.mjs";
+
+export default new Mod(
+	"Entity Texture Features",
+	"Traben_0",
+	"A Fabric mod implementing randomized & emissive texture support for mobs set by the resourcepack. Fully compatible with the Optifine format & Sodium.",
+)
+.add_version(16, 17, 18)
+.add_category("Cosmetic")
+.add_link(
+	{ host: "curseforge", params: { namespace: "entity-texture-features-fabric" } },
+  { host: "modrinth" },
+	{ host: "github" }
+);
