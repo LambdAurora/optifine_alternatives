14d534db10eaf7295db32b191e8d3edcee28eb05
LambdAurora <email@lambdaurora.dev>
2022-03-17 00:37:57 +0100

    Tweak style and add Quick Spyglasser.

--- alternatives/quick-spyglasser.mjs
+++ alternatives/quick-spyglasser.mjs
@@ -1,15 +1,14 @@
 import Mod from "../build_src/mod.mjs";
 
 export default new Mod(
-	"Zoomify",
-	"isXander",
-	"A rather simple zoom mod with moderate customizability.",
+	"Quick Spyglasser",
+	"Gluton",
+	"Adds a client-side keybind for using a spyglass anywhere in your inventory.",
 )
 .icon("https://cdn.modrinth.com/data/w7ThoJFB/25d48c335340c12566044c8f35df5102e72dc06c.png")
-.add_version(18)
+.add_version(17)
 .add_category("Utility", "Zoom")
 .add_link(
 	{ host: "curseforge" },
-	{ host: "modrinth" },
-	{ host: "github" }
+	{ host: "github", params: { author: "Gluton-Official", namespace: "QuickSpyglasser" } }
 );
