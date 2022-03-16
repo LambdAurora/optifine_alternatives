8d551d248dfce5c2f7e4adf0be972cb1b5ffc67d
LambdAurora <email@lambdaurora.dev>
2022-03-17 00:43:03 +0100

    Fix wrong icon.

--- alternatives/quick-spyglasser.mjs
+++ alternatives/quick-spyglasser.mjs
@@ -5,7 +5,7 @@ export default new Mod(
 	"Gluton",
 	"Adds a client-side keybind for using a spyglass anywhere in your inventory.",
 )
-.icon("https://cdn.modrinth.com/data/w7ThoJFB/25d48c335340c12566044c8f35df5102e72dc06c.png")
+.icon("https://media.forgecdn.net/avatars/thumbnails/395/489/64/64/637593852239594205.png")
 .add_version(17)
 .add_category("Utility", "Zoom")
 .add_link(
