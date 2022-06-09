2743947242c3ba29884a843b146a18ddb0918951
LambdAurora <email@lambdaurora.dev>
2022-06-09 19:22:00 +0200

    Update LambDynamicLights metadata.

--- alternatives/lambdynamiclights.mjs
+++ alternatives/lambdynamiclights.mjs
@@ -6,7 +6,7 @@ export default new Mod(
 	"Adds dynamic lighting to the game.",
 )
 .icon("https://cdn.modrinth.com/data/yBW8D80W/icon.png")
-.add_version(15, 16, 17, 18)
+.add_version(15, 16, 17, 18, 19)
 .add_category("Cosmetic")
 .add_link(
 	{ host: "curseforge" },
