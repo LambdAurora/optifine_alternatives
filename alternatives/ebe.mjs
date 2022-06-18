2f84633276fd23064d224dd608f83f77f7f83f27
Finlay Davidson <finlay.davidson@coderclass.nl>
2022-06-18 21:08:40 +0200

    Update Enhanced Block Entities to 1.19

--- alternatives/ebe.mjs
+++ alternatives/ebe.mjs
@@ -6,7 +6,7 @@ export default new Mod(
 	"Reduce FPS lag with block entities, as well as customize them with resource packs.",
 )
 .icon("https://cdn.modrinth.com/data/OVuFYfre/icon.png")
-.add_version(16, 17, 18)
+.add_version(16, 17, 18, 19)
 .add_category("Performance", "Client")
 .add_link(
 	{ host: "curseforge", params: { namespace: "enhanced-block-entities" } },
