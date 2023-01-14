825656e4f502601f8b91b03851532c3728dceb3b
osfanbuff63 <osfanbuff63@osfanbuff63.tech>
2023-01-14 17:17:37 -0500

    Fix Ok Zoomer description
    We might want to use something like `The ultimate zoom mod, accept no substitutes` but I'm not sure there, so I've gone ahead and submitted it anyway.

--- alternatives/okzoomer.mjs
+++ alternatives/okzoomer.mjs
@@ -3,7 +3,7 @@ import Mod from "../build_src/mod.mjs";
 export default new Mod(
 	"Ok Zoomer",
 	"EnnuiL",
-	"Adds a highly-configurable zoom key for Fabric. The zoom is yours!",
+	"Adds a highly-configurable zoom key for Quilt. The zoom is yours!",
 )
 .icon("https://cdn.modrinth.com/data/aXf2OSFU/icon.png")
 .add_version({ loader: ["quilt"], v: [18, 19.3] }, { loader: ["fabric"], v: [14, 15, 16, 17, 18] })
