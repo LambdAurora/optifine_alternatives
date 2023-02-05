1a597c49517018f756d6d70777048c296c68c2c8
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2023-02-05 19:13:41 +0200

    Create no-telemetry.mjs

---
+++ alternatives/no-telemetry.mjs
@@ -0,0 +1,14 @@
+import Mod from "../build_src/mod.mjs";
+
+export default new Mod(
+	"No Telemetry",
+	"kb1000",
+	"No Telemetry is a mod that allows you to disable Microsofts data tracking which has been added in the snapshot 21w38a.",
+)
+.icon("https://cdn-raw.modrinth.com/data/hg77g4Pw/icon.png")
+.add_version({ loader: ["fabric", "forge", "quilt"], v: [18, 19.3] })
+.add_category("Utility", "Zoom")
+.add_link(
+	{ host: "modrinth"},
+	{ host: "curseforge"}
+);
