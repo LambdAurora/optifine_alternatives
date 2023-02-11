ffad27d9ea91d225c83566a4882cf1de2cb2e2d4
LambdAurora <email@lambdaurora.dev>
2023-02-11 23:33:59 +0100

    Update LBG data.

--- alternatives/lambdabettergrass.mjs
+++ alternatives/lambdabettergrass.mjs
@@ -6,7 +6,7 @@ export default new Mod(
 	"Adds better grass and snow to the game.",
 )
 .icon("https://cdn.modrinth.com/data/2Uev7LdA/icon.png")
-.add_version({ loader: ["quilt"], v: [18, 19.2] }, { loader: ["fabric"], v: [16, 17, 18, 19.2] })
+.add_version({ loader: ["quilt"], v: [18, 19.3] }, { loader: ["fabric"], v: [16, 17, 18, 19.2] })
 .add_category("Cosmetic")
 .add_link(
 	{ host: "modrinth" },
