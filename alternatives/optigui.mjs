c87a6cbc3ff9ef26875ef07ecfc690b49f0fa1de
opekope2 <opekope2@gmail.com>
2023-06-13 02:25:26 +0200

    Update optigui.mjs
    Mark as 1.20.1-supported

--- alternatives/optigui.mjs
+++ alternatives/optigui.mjs
@@ -6,7 +6,7 @@ export default new Mod(
 	"Custom inventory GUIs on Fabric&Quilt with OptiFine resource pack support&many more features",
 )
 .icon("https://cdn-raw.modrinth.com/data/JuksLGBQ/2ec739011e612256ebe0c79a4ce064d0e453dd0f.png")
-.add_version({ loader: ["fabric", "quilt"], v: [18, 20] })
+.add_version({ loader: ["fabric", "quilt"], v: [18, 20.1] })
 .add_category("Cosmetic")
 .add_link(
 	{ host: "modrinth" },
