193bfddfc1f4b58c7b67df48a2962c316a905ab9
LambdAurora <email@lambdaurora.dev>
2024-02-05 19:17:51 +0100

    Tweak a little bit polytone entry.

--- alternatives/polytone.mjs
+++ alternatives/polytone.mjs
@@ -1,25 +1,24 @@
 import Mod from "../build_src/mod.mjs";
 
-const HOST_ID = "polytone";
-
 const mod = new Mod(
 	"Polytone",
 	"MehVahdJukaar",
-	"Multipurpose Visual Customization mod: tweak Colors, Colormaps, Lightmaps, Sounds, Gui Elements, Biome Effects, Particles, Fog Colors and more. Supports old Optifine formats",
+	"Multipurpose Visual Customization mod: tweak colors, colormaps, lightmaps, sounds, GUI elements, biome effects, particles, fog colors and more."
+	+ "Supports old Optifine formats",
 )
 .icon("https://github.com/MehVahdJukaar/polytone/blob/master/forge/src/main/resources/icon.png?raw=true")
 .add_version(
-	{ loader: ["fabric"], v: [18, 19, 20.4] },
-	{ loader: ["neoforge"], v: [19, 20.4] },
-	{ loader: ["forge"], v: [18, 19, 20.1] })
-.add_category("Cosmetic", "Fog")
+	{ loader: ["fabric"], v: [20.2, 20.3, 20.4] },
+	{ loader: ["quilt"], v: [18.2, 19.2, 20.3, 20.4] },
+	{ loader: ["neoforge"], v: [20.2, 20.3, 20.4] },
+	{ loader: ["forge"], v: [20.2, 20.3, 20.4] }
+).add_category("Cosmetic")
 .add_link(
-	{ host: "modrinth", params: { namespace: HOST_ID } },
-	{ host: "curseforge", params: { namespace: HOST_ID } },
-	{ host: "github", params: { namespace: HOST_ID }}
+	{ host: "modrinth" },
+	{ host: "curseforge" },
+	{ host: "github" }
 );
 
-await mod.import_versions_from_modrinth(HOST_ID);
-
+await mod.import_versions_from_modrinth();
 
 export default mod;
