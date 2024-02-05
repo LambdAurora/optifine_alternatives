160b4af1143244b3886d46791233522d558ead84
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-02-05 14:57:45 +0100

    fog

--- alternatives/polytone.mjs
+++ alternatives/polytone.mjs
@@ -5,14 +5,14 @@ const HOST_ID = "polytone";
 const mod = new Mod(
 	"Polytone",
 	"MehVahdJukaar",
-	"Multipurpose Visual Customization mod: tweak Colors, Colormaps, Lightmaps, Sounds, Gui Elements, Biome Effects, Particles and more. Supports old Optifine formats",
+	"Multipurpose Visual Customization mod: tweak Colors, Colormaps, Lightmaps, Sounds, Gui Elements, Biome Effects, Particles, Fog Colors and more. Supports old Optifine formats",
 )
 .icon("https://github.com/MehVahdJukaar/polytone/blob/master/forge/src/main/resources/icon.png?raw=true")
 .add_version(
 	{ loader: ["fabric"], v: [18, 19, 20.4] },
 	{ loader: ["neoforge"], v: [19, 20.4] },
 	{ loader: ["forge"], v: [18, 19, 20.1] })
-.add_category("Cosmetic")
+.add_category("Cosmetic", "Fog")
 .add_link(
 	{ host: "modrinth", params: { namespace: HOST_ID } },
 	{ host: "curseforge", params: { namespace: HOST_ID } },
