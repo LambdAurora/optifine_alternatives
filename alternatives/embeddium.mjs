ac0739c6219401daf066a6daa884e6602f16f677
OKHI233 <43093801+OKHI233@users.noreply.github.com>
2024-02-28 00:03:54 +0800

    Update embeddium.mjs

--- alternatives/embeddium.mjs
+++ alternatives/embeddium.mjs
@@ -6,7 +6,7 @@ const mod = new Mod(
 	"A performance-enhancing client mod (based on Sodium) prioritizing reliability & mod compatibility",
 )
 .icon("https://cdn.modrinth.com/data/sk9rgfiA/7f5be8843494e3c34bd628143cbb07bc6cbc77f7.png")
-.add_version({ loader: ["fabric", "quilt"], v: [20.1, 20.4] }, { loader: ["forge"], v: [16.5, 18.2, 19.2, 20] }, { loader: ["neoforge"], v: [20] }
+.add_version({ loader: ["fabric", "quilt"], v: [20.1, 20.4] }, { loader: ["forge"], v: [16.5, 18.2, 19.2, 20.1, 20.2] }, { loader: ["neoforge"], v: [20] })
 .add_category("Performance", "Client")
 .add_link(
 	{ host: "modrinth" },
