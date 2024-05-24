0d0508a910788ab412a05ec0b76592b4cbbb2720
Lilly Rose Berner <lilly@lostluma.net>
2024-05-24 22:04:15 +0200

    Update Dynamic FPS supported versions and metadata (#76)

--- alternatives/dynamic-fps.mjs
+++ alternatives/dynamic-fps.mjs
@@ -2,11 +2,12 @@ import Mod from "../build_src/mod.mjs";
 
 const mod = new Mod(
 	"Dynamic FPS",
-	"juliand665",
-	"Improve performance when Minecraft is in the background.",
+	"juliand665, LostLuma",
+	"Reduce resource usage while Minecraft is in the background or idle.",
 )
 .icon("https://cdn.modrinth.com/data/LQ3K71Q1/icon.png")
-.add_version({ loader: ["quilt"], v: [18, 19, 20] }, { loader: ["fabric"], v: [14, 15, 16, 17, 18, 19, 20] }, { loader: ["forge"], v: [16.5, 17, 18, 19, 20] }, { loader: ["neoforge"], v: [20] })
+// Some early 1.14.x and 1.16.x Fabric / Quilt versions are only available on GitHub and CurseForge, so the import doesn't catch them
+.add_version({ loader: ["fabric", "quilt"], v: [14, 14.1, 14.2, 15, 16, 16.1, 17, 18, 19, 20] }, { loader: ["forge"], v: [16.5, 17, 18, 19, 20] }, { loader: ["neoforge"], v: [20.2, 20.3, 20.4] })
 .add_category("Performance", "Client")
 .add_link(
 	{ host: "modrinth" },
